949c1a0b1a3961a6cd9feb8581c7f2b8e1e23fa0
Andre Braga Reis <andre@brg.rs>
2021-02-07 00:29:13 +0000

    [wip] lib.rs first pass

--- rust/datafusion/src/lib.rs
+++ rust/datafusion/src/lib.rs
@@ -30,7 +30,7 @@
 //! as well as a query optimizer and execution engine capable of parallel execution
 //! against partitioned data sources (CSV and Parquet) using threads.
 //!
-//! Below is an example of how to execute a query against a CSV using [`DataFrames`](dataframe::DataFrame):
+//! Below is an example of how to execute a query against a CSV using [`DataFrame`]s:
 //!
 //! ```rust
 //! # use datafusion::prelude::*;
@@ -83,14 +83,14 @@
 //! Specifically, when DataFusion receives an SQL query, there are different steps
 //! that it passes through until a result is obtained. Broadly, they are:
 //!
-//! 1. The string is parsed to an Abstract syntax tree (AST) using [sqlparser](https://docs.rs/sqlparser/0.6.1/sqlparser/).
-//! 2. The planner [`SqlToRel`](sql::planner::SqlToRel) converts logical expressions on the AST to logical expressions [`Expr`s](logical_plan::Expr).
-//! 3. The planner [`SqlToRel`](sql::planner::SqlToRel) converts logical nodes on the AST to a [`LogicalPlan`](logical_plan::LogicalPlan).
-//! 4. [`OptimizerRules`](optimizer::optimizer::OptimizerRule) are applied to the [`LogicalPlan`](logical_plan::LogicalPlan) to optimize it.
-//! 5. The [`LogicalPlan`](logical_plan::LogicalPlan) is converted to an [`ExecutionPlan`](physical_plan::ExecutionPlan) by a [`PhysicalPlanner`](physical_plan::PhysicalPlanner)
-//! 6. The [`ExecutionPlan`](physical_plan::ExecutionPlan) is executed against data through the [`ExecutionContext`](execution::context::ExecutionContext)
+//! 1. The string is parsed to an Abstract syntax tree (AST) using [sqlparser](https://docs.rs/sqlparser/).
+//! 2. The planner [`SqlToRel`] converts logical expressions on the AST to logical expressions [`Expr`]s
+//! 3. The planner [`SqlToRel`] converts logical nodes on the AST to a [`LogicalPlan`].
+//! 4. [`OptimizerRule`]s are applied to the [`LogicalPlan`] to optimize it.
+//! 5. The [`LogicalPlan`] is converted to an [`ExecutionPlan`] by a [`PhysicalPlanner`]
+//! 6. The [`ExecutionPlan`] is executed against data through the [`ExecutionContext`]
 //!
-//! With a [`DataFrame`](dataframe::DataFrame) API, steps 1-3 are not used as the DataFrame builds the [`LogicalPlan`](logical_plan::LogicalPlan) directly.
+//! With a [`DataFrame`] API, steps 1-3 are not used as the DataFrame builds the [`LogicalPlan`] directly.
 //!
 //! Phases 1-5 are typically cheap when compared to phase 6, and thus DataFusion puts a
 //! lot of effort to ensure that phase 6 runs efficiently and without errors.
@@ -99,59 +99,91 @@
 //!
 //! ### Logical plan
 //!
-//! Logical planning yields [`logical plans`](logical_plan::LogicalPlan) and [`logical expressions`](logical_plan::Expr).
-//! These are [`Schema`](arrow::datatypes::Schema)-aware traits that represent statements whose result is independent of how it should physically be executed.
+//! Logical planning yields [logical plans](logical_plan::LogicalPlan) and [logical expressions](logical_plan::Expr).
+//! These are [`Schema`]-aware traits that represent statements whose result is independent of how it should physically be executed.
 //!
-//! A [`LogicalPlan`](logical_plan::LogicalPlan) is a Direct Asyclic graph of other [`LogicalPlan`s](logical_plan::LogicalPlan) and each node contains logical expressions ([`Expr`s](logical_plan::Expr)).
-//! All of these are located in [`logical_plan`](logical_plan).
+//! A [`LogicalPlan`] is a Direct Acyclic graph of other [`LogicalPlan`]s and each node contains logical expressions ([`Expr`]s).
+//! All of these are located in [`logical_plan`].
 //!
 //! ### Physical plan
 //!
-//! A Physical plan ([`ExecutionPlan`](physical_plan::ExecutionPlan)) is a plan that can be executed against data.
+//! A Physical plan ([`ExecutionPlan`]) is a plan that can be executed against data.
 //! Contrarily to a logical plan, the physical plan has concrete information about how the calculation
 //! should be performed (e.g. what Rust functions are used) and how data should be loaded into memory.
 //!
-//! [`ExecutionPlan`](physical_plan::ExecutionPlan) uses the Arrow format as its in-memory representation of data, through the [arrow] crate.
+//! [`ExecutionPlan`] uses the Arrow format as its in-memory representation of data, through the [arrow] crate.
 //! We recommend going through [its documentation](arrow) for details on how the data is physically represented.
 //!
-//! A [`ExecutionPlan`](physical_plan::ExecutionPlan) is composed by nodes (implement the trait [`ExecutionPlan`](physical_plan::ExecutionPlan)),
-//! and each node is composed by physical expressions ([`PhysicalExpr`](physical_plan::PhysicalExpr))
-//! or aggreagate expressions ([`AggregateExpr`](physical_plan::AggregateExpr)).
-//! All of these are located in the module [`physical_plan`](physical_plan).
+//! An [`ExecutionPlan`] is composed by nodes (implement the trait [`ExecutionPlan`]),
+//! and each node is composed by physical expressions ([`PhysicalExpr`])
+//! or aggregate expressions ([`AggregateExpr`]).
+//! All of these are located in the module [`physical_plan`].
 //!
 //! Broadly speaking,
 //!
-//! * an [`ExecutionPlan`](physical_plan::ExecutionPlan) receives a partition number and asyncronosly returns
-//!   an iterator over [`RecordBatch`](arrow::record_batch::RecordBatch)
-//!   (a node-specific struct that implements [`RecordBatchReader`](arrow::record_batch::RecordBatchReader))
-//! * a [`PhysicalExpr`](physical_plan::PhysicalExpr) receives a [`RecordBatch`](arrow::record_batch::RecordBatch)
-//!   and returns an [`Array`](arrow::array::Array)
-//! * an [`AggregateExpr`](physical_plan::AggregateExpr) receives [`RecordBatch`es](arrow::record_batch::RecordBatch)
-//!   and returns a [`RecordBatch`](arrow::record_batch::RecordBatch) of a single row(*)
+//! * an [`ExecutionPlan`] receives a partition number and asynchronosly returns
+//!   an iterator over [`RecordBatch`]
+//!   (a node-specific struct that implements [`RecordBatchReader`])
+//! * a [`PhysicalExpr`] receives a [`RecordBatch`]
+//!   and returns an [`Array`]
+//! * an [`AggregateExpr`] receives [`RecordBatch`]es
+//!   and returns a [`RecordBatch`] of a single row<sup>†</sup>
 //!
-//! (*) Technically, it aggregates the results on each partition and then merges the results into a single partition.
+//! *<sup>†</sup> technically, it aggregates the results on each partition and then merges the results into a single partition*
 //!
 //! The following physical nodes are currently implemented:
 //!
-//! * Projection: [`ProjectionExec`](physical_plan::projection::ProjectionExec)
-//! * Filter: [`FilterExec`](physical_plan::filter::FilterExec)
-//! * Hash and Grouped aggregations: [`HashAggregateExec`](physical_plan::hash_aggregate::HashAggregateExec)
-//! * Sort: [`SortExec`](physical_plan::sort::SortExec)
-//! * Merge (partitions): [`MergeExec`](physical_plan::merge::MergeExec)
-//! * Limit: [`LocalLimitExec`](physical_plan::limit::LocalLimitExec) and [`GlobalLimitExec`](physical_plan::limit::GlobalLimitExec)
-//! * Scan a CSV: [`CsvExec`](physical_plan::csv::CsvExec)
-//! * Scan a Parquet: [`ParquetExec`](physical_plan::parquet::ParquetExec)
-//! * Scan from memory: [`MemoryExec`](physical_plan::memory::MemoryExec)
-//! * Explain the plan: [`ExplainExec`](physical_plan::explain::ExplainExec)
+//! * Projection: [`ProjectionExec`]
+//! * Filter: [`FilterExec`]
+//! * Hash and Grouped aggregations: [`HashAggregateExec`]
+//! * Sort: [`SortExec`]
+//! * Merge (partitions): [`MergeExec`]
+//! * Limit: [`LocalLimitExec`] and [`GlobalLimitExec`]
+//! * Scan a CSV: [`CsvExec`]
+//! * Scan a Parquet: [`ParquetExec`]
+//! * Scan from memory: [`MemoryExec`]
+//! * Explain the plan: [`ExplainExec`]
 //!
 //! ## Customize
 //!
 //! DataFusion allows users to
-//! * extend the planner to use user-defined logical and physical nodes ([`QueryPlanner`](execution::context::QueryPlanner))
-//! * declare and use user-defined scalar functions ([`ScalarUDF`](physical_plan::udf::ScalarUDF))
-//! * declare and use user-defined aggregate functions ([`AggregateUDF`](physical_plan::udaf::AggregateUDF))
+//! * extend the planner to use user-defined logical and physical nodes ([`QueryPlanner`])
+//! * declare and use user-defined scalar functions ([`ScalarUDF`])
+//! * declare and use user-defined aggregate functions ([`AggregateUDF`])
 //!
-//! you can find examples of each of them in examples section.
+//! You can find examples of each of them in examples section.
+//!
+//! [`AggregateExpr`]: physical_plan::AggregateExpr
+//! [`AggregateExpr`]: physical_plan::AggregateExpr
+//! [`AggregateUDF`]: physical_plan::udaf::AggregateUDF
+//! [`Array`]: arrow::array::Array
+//! [`CsvExec`]: physical_plan::csv::CsvExec
+//! [`DataFrame`]: dataframe::DataFrame
+//! [`ExecutionContext`]: execution::context::ExecutionContext
+//! [`ExecutionPlan`]: physical_plan::ExecutionPlan
+//! [`ExplainExec`]: physical_plan::explain::ExplainExec
+//! [`Expr`]: logical_plan::Expr
+//! [`FilterExec`]: physical_plan::filter::FilterExec
+//! [`GlobalLimitExec`]: physical_plan::limit::GlobalLimitExec
+//! [`HashAggregateExec`]: physical_plan::hash_aggregate::HashAggregateExec
+//! [`LocalLimitExec`]: physical_plan::limit::LocalLimitExec
+//! [`LogicalPlan`]: logical_plan::LogicalPlan
+//! [`logical_plan`]: logical_plan
+//! [`MemoryExec`]: physical_plan::memory::MemoryExec
+//! [`MergeExec`]: physical_plan::merge::MergeExec
+//! [`OptimizerRule`]: optimizer::optimizer::OptimizerRule
+//! [`ParquetExec`]: physical_plan::parquet::ParquetExec
+//! [`PhysicalExpr`]: physical_plan::PhysicalExpr
+//! [`PhysicalPlanner`]: physical_plan::PhysicalPlanner
+//! [`physical_plan`]: physical_plan
+//! [`ProjectionExec`]: physical_plan::projection::ProjectionExec
+//! [`QueryPlanner`]: execution::context::QueryPlanner
+//! [`RecordBatchReader`]: arrow::record_batch::RecordBatchReader
+//! [`RecordBatch`]: arrow::record_batch::RecordBatch
+//! [`ScalarUDF`]: physical_plan::udf::ScalarUDF
+//! [`Schema`]: arrow::datatypes::Schema
+//! [`SortExec`]: physical_plan::sort::SortExec
+//! [`SqlToRel`]: sql::planner::SqlToRel
 
 extern crate arrow;
 extern crate sqlparser;
